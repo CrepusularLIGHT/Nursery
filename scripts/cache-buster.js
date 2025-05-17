d73bbea5ae4bde2b992c66f1b4eb143530f96ccf
Ra <jesse.rockstar26@gmail.com>
2025-05-16 21:08:41 -0600

    cache bust and css

--- scripts/cache-buster.js
+++ scripts/cache-buster.js
@@ -1,7 +1,7 @@
 // This is for updating the version on the js and css so that it will actually load everytime instead of just being blank and lame.
 
 // Generate a version number (timestamp)
-const version = new Date().getTime();
+// const version = new Date().getTime();
 
 // Get the CSS file and update its href
 // const cssFile = document.getElementById("css-file");
